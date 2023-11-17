b53b57eb124ee3a411a91432c335d60df2d1ce6e
MarkusSavotskin <markus.savotskin@voco.ee>
2023-11-17 16:26:15 +0200

    Created search with POST method

--- src/Controller/AlbumsController.php
+++ src/Controller/AlbumsController.php
@@ -21,11 +21,27 @@ class AlbumsController extends Controller
 
         if ($query) {
             $albums = array_values(array_filter($albums, function($album) use ($query) {
-                return strpos($album['title'], $query) !== false or strpos($album['artist'], $query) !== false;
+                return strpos(strtolower($album['title']), strtolower($query)) !== false or strpos(strtolower($album['artist']), strtolower($query)) !== false;
             }));
         }
 
         return $this->render($response, 'search.html', [
+            'query' => $query,
+            'albums' => $albums]);
+    }
+
+    public function form(Request $request, Response $response)
+    {
+        $albums = json_decode(file_get_contents(__DIR__ .'/../../data/albums.json'), true);
+        $query = $request->getParam('q');
+
+        if ($request->isPost()) {
+            $albums = array_values(array_filter($albums, function($album) use ($query) {
+                return strpos(strtolower($album['title']), strtolower($query)) !== false or strpos(strtolower($album['artist']), strtolower($query)) !== false;
+            }));
+        }
+
+        return $this->render($response, 'form.html', [
             'query' => $query,
             'albums' => $albums]);
     }
